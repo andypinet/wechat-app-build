c316d9a2eb0c15bc91f23086553ea2c8c3e22495
andypinet <1585638808@qq.com>
2018-12-06 15:41:08 +0800

    添加doc

--- src/behaviors/mt-form.behavior.js
+++ src/behaviors/mt-form.behavior.js
@@ -1,3 +1,24 @@
+// wxml
+//
+// <form
+// bindsubmit="_mtFormSubmit"
+// bindreset="_mtFormReset">
+//   <input type="text" name="name"/>
+//   <button
+// form-type="submit"
+// custom-class="a-btn-default">submit
+//   </button>
+//   </form>
+
+// js
+//
+// const mtFormBehvior = require('../../behaviors/mt-form.behavior')
+
+// handle
+//
+// mtValidateSchema 返回每次调用的schema
+// mtOnFormSubmit 当form表单提交时回调
+
 const app = getApp()
 
 let validate = app.validate
